e64f39119d278eb7144667685680a0cc9b08a901
evotopid <evotopid@gmail.com>
2014-04-18 18:01:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Griddie Theme for Tumblr
+Hi There.
+This is my first Tumblr theme to be published.
+Currently it works, but I can't submit it to the Theme Garden for some reason yet…
+In the meantime you can see it work on my [theme demo blog.](http://griddie-theme.tumblr.com)
+
+## Information
+For the licenses of the libraries, please check the `libraries.js` file.
+
+
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Griddie Theme</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://github.com/evotopid/griddie-theme" property="cc:attributionName" rel="cc:attributionURL">evotopid</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
---
+++ theme.js
@@ -0,0 +1,52 @@
+var $state = new Object;
+$state.loadingPosts = false;
+$state.nextPage = undefined; // loaded inside document.ready
+var $posts;
+$state.morePostsAvailable = function() {
+  return (!$state.loadingPosts && $state.nextPage != undefined);
+};
+
+function updatePage() {
+  if ($state.morePostsAvailable() && ($(document).height() - $(window).height()) - $(window).scrollTop() < 200) {
+    $state.loadingPosts = true;
+    $("#loader").show();
+    $.ajax({
+      url: $state.nextPage,
+      dataType: "html",
+      success: function(html) {
+        $state.nextPage = $("#nextpage", html).attr("href");
+        posts = $(".post", html);
+        $("#temp_stack").append(posts);
+        $("#temp_stack > .post").each(function() {
+          $(this).imagesLoaded(function(instance) {
+            element = instance.elements[0];
+            $posts.append(element);
+            $posts.masonry('appended', element);
+            if ($("#temp_stack").children().length == 0) {
+              $("#loader").hide();
+              $state.loadingPosts = false;
+            }
+          });
+        });
+      }
+    });
+  }
+  setTimeout(function() {
+    updatePage();
+  }, 100);
+}
+window.startThemeJS = function() {
+  $posts = $("#posts");
+  $("#loader").hide();
+  $("#nextpage").hide();
+  imagesLoaded("#posts", function() {
+    $posts.masonry({
+      columnWidth: 250,
+      itemSelector: '.post',
+      gutter: 5,
+      transitionDuration: 0
+    });
+    $state.nextPage = $("#nextpage").attr("href");
+    updatePage();
+  });
+};
